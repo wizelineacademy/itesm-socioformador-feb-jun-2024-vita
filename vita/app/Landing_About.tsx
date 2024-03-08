255299f3225eec69f0641e449354b2711f58e48a
CosmicBlue <A00833278@tec.mx>
2024-03-08 09:23:21 -0600

    About Page changes - Font Size

--- vita/app/Landing_About.tsx
+++ vita/app/Landing_About.tsx
@@ -10,14 +10,13 @@ const HomePage: React.FC = () => {
   };
 
   return (
-    <div style={{ backgroundImage: "url('AboutBG.png')", backgroundSize: 'cover', minHeight: '100vh' }}>
-        <div id='Flex-Container' className='flex flex-col justify-center gap-80'>
-            <div>Nav Bar</div>
+    <div className='flex flex-col justify-center align-middle' style={{ backgroundImage: "url('AboutBG.png')", backgroundSize: 'cover', minHeight: '100vh'  }}>
+        <div id='Flex-Container' >
             <div id='About_Container' className='flex justify-center items-center flex-col gap-12'>
-                <h1 className="text-9xl font-bold mb-5">Acerca De</h1>
+                <h1 className="text-6xl font-bold mb-5">Acerca De</h1>
                 <Image src={"/Line.png"} alt='Line divider' width={300} height={10}></Image>
 
-                <h2 className='text-5xl font-bold  mb-5 text-center	'><span className='text-red-300	3xl font-bold'>VITA</span> es una aplicación para <br /> monitorear tu salud a todo momento y <br /> recibir asesoramiento a través de  <br />inteligencia artificial y expertos en la <br />salud. </h2>
+                <h2 className='text-4xl font-bold  mb-5 text-center	'><span className='text-red-300	4xl font-bold'>VITA</span> es una aplicación para <br /> monitorear tu salud a todo momento y <br /> recibir asesoramiento a través de  <br />inteligencia artificial y expertos en la <br />salud. </h2>
             </div>
         </div>
         
