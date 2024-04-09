039da2ed5115fb6388f4241fae8c14db9f729998
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2024-04-09 10:16:14 -0600

    Style: Heart

--- vita/app/(landing)/Home.tsx
+++ vita/app/(landing)/Home.tsx
@@ -1,39 +1,36 @@
-"use client"
 import React from 'react';
 import Image from 'next/image';
 import Button from "../components/Button";
 import Information from '../components/information/Information';
 
-
 const Home: React.FC = () => {
 
   return (
-    <div id="Container" className="flex justify-center items-center flex-wrap h-screen">
-            <div id="Left Information" className="flex flex-col items-start px-20"> 
-                <Information />
-                <div className="pt-4">
-                    <Button 
-                        borderColor="border-custom-red"
-                        label= "Regístrate" 
-                        outline
-                        big
-                        onClick={() => {}}
-                      
-                    />  
-                </div>
-            </div>
+    <div id="Container" className="flex flex-col md:flex-row lg:flex-row">
+      <div id="Left Information" className="flex flex-col items-start px-20"> 
+        <Information />
+        <div className="pt-4">
+          <Button 
+            borderColor="border-custom-red"
+            label= "Regístrate" 
+            outline
+            big
+            onClick={() => {}}
+          />  
+        </div>
+      </div>
 
-            <div id="Right Image">
-            <Image
-                src="/heart.svg"
-                width={1000}
-                height={1000}
-                alt="Picture of the author"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                
-            />
-            </div>
-        </div>  
+      <div id="Right Image" className='pl-20'>
+        <Image
+          src="/heart.svg"
+          className="md:w-700 lg:w-600" // Tamaño condicional de la imagen
+          width={700}
+          height={600}
+          alt="Picture of the author"
+        />
+      </div>
+            
+    </div>  
   );
 };
 
