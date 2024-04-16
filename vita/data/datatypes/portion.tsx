3a04f2b886f8f18b0ad1ea1a33793f7d0b2cf6c1
julio2505 <juliomeza2510@outlook.com>
2024-04-15 21:45:39 -0600

    chore: Added portions datatypes

---
+++ vita/data/datatypes/portion.tsx
@@ -0,0 +1,25 @@
+export interface Portion {
+    fruits: number,
+    vegetables: number,
+    legumes: number,
+    meat: number,
+    milk: number,
+    cereals: number,
+    sugar: number,
+    fat: number
+}
+
+export interface CaloriesPortion {
+    [key: number]: Portion
+}
+
+export interface PlanPortions {
+    increase: CaloriesPortion,
+    decrease: CaloriesPortion
+}
+
+export interface GenderPortions {
+    "M": PlanPortions,
+    "F": PlanPortions
+}
+
