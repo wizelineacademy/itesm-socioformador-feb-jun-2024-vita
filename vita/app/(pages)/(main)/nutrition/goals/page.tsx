23992a771ec39c1c969296d4e39524a394204f02
julio2505 <juliomeza2510@outlook.com>
2024-04-23 10:56:41 -0600

    feat: Added goal visualization

--- vita/app/(pages)/(main)/nutrition/goals/page.tsx
+++ vita/app/(pages)/(main)/nutrition/goals/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import ButtonEvaluation from '@/components/buttons/ButtonEvaluation.';
+import MainButton from '@/components/buttons/MainButton';
 import React, { useState, useEffect } from 'react';
 
 const GoalsPage = () => {
@@ -24,25 +26,22 @@ const GoalsPage = () => {
   };
 
   return (
-    <div className="h-screen overflow-auto bg-[#2C0521] p-4 text-white flex flex-col items-start justify-start space-y-4 pt-10">
+    <div className="h-screen overflow-auto bg-[#2C0521] p-4 text-white flex flex-col items-start justify-start space-y-4 pt-10 md:items-start">
       <h2 className="text-5xl font-bold mb-4">Mi Meta</h2>
 
       {!goal && (
-        <>
-          <p className="text-xl">No tienes ninguna meta configurada actualmente.</p>
-          <button onClick={() => setShowDropdown(true)} className="mt-4 px-4 py-2 bg-blue-500 rounded-full text-xl">
-            Crear Meta
-          </button>
-        </>
+        <div className='flex flex-col w-full gap-y-5 lg:items-start'>
+          <p className="text-xl font-bold">No tienes ninguna meta configurada actualmente.</p>
+          <ButtonEvaluation onClick={() => {setShowDropdown(true)}} text='Crear meta'/>
+        </div>
       )}
 
       {goal && (
-        <>
-          <p className="text-xl">Tu meta actual es: <span className="font-bold">{goal}</span></p>
-          <button onClick={handleEditGoal} className="px-4 py-2 bg-yellow-400 rounded-full text-xl">
-            Editar Meta
-          </button>
-        </>
+          <div className='flex flex-col w-full gap-y-5 lg:items-start'>
+            <p className="text-xl font-bold">Tu meta actual es:</p>
+            <p className='mt-5 py-3 pl-4 w-full max-w-[500px] rounded-2xl text-lg bg-custom-lightpurple'>{goal}</p>
+            <ButtonEvaluation onClick={() => {setShowDropdown(true)}} text='Editar meta'/>
+          </div>
       )}
 
       {showDropdown && (
