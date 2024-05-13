e7826bda3151a0e56098c0ce1f063f7daa4a9880
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2024-05-13 13:55:20 -0600

    style: Add loading

--- vita/app/(pages)/(main)/nutrition/imageDetection/page.tsx
+++ vita/app/(pages)/(main)/nutrition/imageDetection/page.tsx
@@ -16,6 +16,7 @@ interface NutritionalInfo {
 function FoodAnalysisPage() {
   const [nutritionalInfos, setNutritionalInfos] = useState<NutritionalInfo[] | null>(null);
   const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false); // Nuevo estado para indicar carga
 
   const handleImageUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -23,9 +24,11 @@ function FoodAnalysisPage() {
 
     const imageUrl = URL.createObjectURL(file);
     setImageUrl(imageUrl);
+    setLoading(true); // Establecer el estado de carga a true
 
     const nutritionalInfos = await getNutritionalInfo(file);
     setNutritionalInfos(nutritionalInfos);
+    setLoading(false); // Establecer el estado de carga a false cuando se completa la carga del modelo
   };
 
   const getNutritionalInfo = async (file: File): Promise<NutritionalInfo[] | null> => {
@@ -83,84 +86,85 @@ para obtener los valores nutricionales de cada alimento identificado.
   **Si la imagen no contiene comida, establezca 'name' en 'Invalido' .**
 `;
 
-    const imagePart = await fileToGenerativePart(file);
+const imagePart = await fileToGenerativePart(file);
 
-    try {
-      const result = await model.generateContent([prompt, imagePart]);
-      const response = await result.response;
-      let text = await response.text();
-      text = text.replaceAll("`", "")
-      text = text.replaceAll("json", "")
-      
-      const nutritionalInfos: NutritionalInfo[] = JSON.parse(text);
+try {
+  const result = await model.generateContent([prompt, imagePart]);
+  const response = await result.response;
+  let text = await response.text();
+  text = text.replaceAll("`", "")
+  text = text.replaceAll("json", "")
+  
+  const nutritionalInfos: NutritionalInfo[] = JSON.parse(text);
 
-      return nutritionalInfos;
-    } catch (error) {
-      console.error(error);
-      return null;
+  return nutritionalInfos;
+} catch (error) {
+  console.error(error);
+  return null;
+}
+};
+
+const fileToGenerativePart = async (file: File) => {
+const base64EncodedDataPromise = new Promise<string>((resolve) => {
+  const reader = new FileReader();
+  reader.onloadend = () => {
+    const result = reader.result;
+    if (typeof result === 'string') {
+      resolve(result.split(',')[1]);
+    } else {
+      resolve('');
     }
   };
+  reader.readAsDataURL(file);
+});
 
-  const fileToGenerativePart = async (file: File) => {
-    const base64EncodedDataPromise = new Promise<string>((resolve) => {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        const result = reader.result;
-        if (typeof result === 'string') {
-          resolve(result.split(',')[1]);
-        } else {
-          resolve('');
-        }
-      };
-      reader.readAsDataURL(file);
-    });
+return {
+  inlineData: { data: await base64EncodedDataPromise, mimeType: file.type },
+};
+};
 
-    return {
-      inlineData: { data: await base64EncodedDataPromise, mimeType: file.type },
-    };
-  };
+const renderNutritionalInfo = () => {
+if (!nutritionalInfos) return null;
 
-  const renderNutritionalInfo = () => {
-    if (!nutritionalInfos) return null;
-
-    return (
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-12 lg:w-3/4">
-        {nutritionalInfos.map((nutritionalInfo, index) => (
-        nutritionalInfo.name === "Invalido"  || nutritionalInfo.name === "Inválido" ? (
-          <p key={index} className="text-3xl font-bold text-white">Imagen Inválida</p>
-        ) : (
-          <div key={index} className="bg-decoration-nutrition-colordark rounded-lg overflow-hidden shadow-md ">
-            <div className="p-6">
-              <h2 className="text-xl font-semibold mb-2 text-white">{nutritionalInfo.name}</h2>
-              <div className="grid grid-cols-2 gap-1">
-                <p className="text-md text-white"><span className="font-semibold">Calorías:</span> {nutritionalInfo.calories}</p>
-                <p className="text-md text-white"><span className="font-semibold">Lípidos:</span> {nutritionalInfo.lipids}</p>
-                <p className="text-md text-white"><span className="font-semibold">Proteínas:</span> {nutritionalInfo.proteins}</p>
-                <p className="text-md text-white "><span className="font-semibold">Carbohidratos:</span> {nutritionalInfo.carbohydrates}</p>
-                <p className="col-span-2 text-md text-white"><span className="font-semibold">Subgrupos:</span> {nutritionalInfo.subgroups.join(", ")}</p>
-              </div>
+return (
+  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-12 lg:w-3/4">
+    {nutritionalInfos.map((nutritionalInfo, index) => (
+      nutritionalInfo.name === "Invalido" || nutritionalInfo.name === "Inválido" ? (
+        <p key={index} className="text-3xl font-bold text-white">Imagen Inválida</p>
+      ) : (
+        <div key={index} className="bg-decoration-nutrition-colordark rounded-lg overflow-hidden shadow-md ">
+          <div className="p-6">
+            <h2 className="text-xl font-semibold mb-2 text-white">{nutritionalInfo.name}</h2>
+            <div className="grid grid-cols-2 gap-1">
+              <p className="text-md text-white"><span className="font-semibold">Calorías:</span> {nutritionalInfo.calories}</p>
+              <p className="text-md text-white"><span className="font-semibold">Lípidos:</span> {nutritionalInfo.lipids}</p>
+              <p className="text-md text-white"><span className="font-semibold">Proteínas:</span> {nutritionalInfo.proteins}</p>
+              <p className="text-md text-white "><span className="font-semibold">Carbohidratos:</span> {nutritionalInfo.carbohydrates}</p>
+              <p className="col-span-2 text-md text-white"><span className="font-semibold">Subgrupos:</span> {nutritionalInfo.subgroups.join(", ")}</p>
             </div>
           </div>
-        )
-      ))}
-      </div>
-    );
-  };
-
-  return (
-    <div className="container p-4">
-      <h1 className="text-2xl font-bold text-start mb-4 text-white">Conteo Calórico </h1>
-      <div className="flex justify-start mb-4">
-        <input type="file" onChange={handleImageUpload} className="px-4 py-2 bg-blue-500 text-white rounded-md cursor-pointer"/>
-      </div>
-      {imageUrl && (
-        <div className="mt-4">
-          <img src={imageUrl} alt="Uploaded" className="max-w-full h-auto sm:w-[500px] sm:h-[300px]" />
         </div>
-      )}
-      {renderNutritionalInfo()}
+      )
+    ))}
+  </div>
+);
+};
+
+return (
+<div className="container p-4">
+  <h1 className="text-2xl font-bold text-start mb-4 text-white">Conteo Calórico </h1>
+  <div className="flex justify-start mb-4">
+    <input type="file" onChange={handleImageUpload} className="px-4 py-2 bg-blue-500 text-white rounded-md cursor-pointer"/>
+  </div>
+  {loading && <p className="text-4xl font-bold text-white">Cargando...</p>} {/* Mostrar el indicador de carga si el estado de carga es true */}
+  {imageUrl && (
+    <div className="mt-4">
+      <img src={imageUrl} alt="Uploaded" className="max-w-full h-auto sm:w-[500px] sm:h-[300px]" />
     </div>
-  );
+  )}
+  {renderNutritionalInfo()}
+</div>
+);
 }
 
 export default FoodAnalysisPage;
