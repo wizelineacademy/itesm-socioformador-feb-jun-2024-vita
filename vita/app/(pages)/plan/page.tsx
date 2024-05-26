03be82f49a24278413704646cfbe3b60554d9f88
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2024-05-25 20:25:33 -0600

    Fix: Router

--- vita/app/(pages)/plan/page.tsx
+++ vita/app/(pages)/plan/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState } from 'react';
 import { NextPage } from 'next';
-import { useRouter } from 'next/router';
+import { useRouter } from "next/navigation";
 
 interface Plan {
   name: string;
