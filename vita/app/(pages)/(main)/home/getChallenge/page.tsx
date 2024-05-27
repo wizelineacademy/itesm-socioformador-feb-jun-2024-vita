e2f2ac56ba40d93ffa95e4ca1c58d8f402ce652a
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2024-05-26 22:44:46 -0600

    feat: add get Monthly Challenges

--- vita/app/(pages)/(main)/home/getChallenge/page.tsx
+++ vita/app/(pages)/(main)/home/getChallenge/page.tsx
@@ -1,17 +1,23 @@
-
 "use client"
 import React, { useState, useEffect } from "react";
 import { FaBell } from 'react-icons/fa';
 import axios from "axios";
 import Swal from "sweetalert2";
+import { getMonthName } from "@/lib/DaysFormat/days";
 
 const GetChallenge = () => {
-  const [challenge, setChallenge] = useState<{ name: string, description: string, startDate: string, endDate: string } | null>(null);
+  const [challenge, setChallenge] = useState<{
+    name: string;
+    description: string;
+    startDate: string;
+    endDate: string;
+  } | null>(null);
 
   useEffect(() => {
     const fetchChallenge = async () => {
       try {
-        const response = await axios.get('/api/challenges');
+       
+        const response = await axios.get(`/api/challenges`);
         setChallenge(response.data);
       } catch (error) {
         Swal.fire({
@@ -26,21 +32,32 @@ const GetChallenge = () => {
     fetchChallenge();
   }, []);
 
+ 
+
   return (
     <div className="flex flex-col px-5 py-4 text-4xl font-bold lg:justify-start md:justify-start justify-center">
-      <span className="flex flex-row"> 
-        <h1 className="mr-2 text-home-title">Reto del Mes</h1>
+      <span className="flex items-center mb-4"> 
+        <h1 className="mr-2 text-home-title">Reto de {getMonthName()}</h1>
         <FaBell size={36} color="white"/>
       </span> 
       {challenge ? (
         <div className="mt-6">
-          <h2 className="text-3xl font-bold mb-4">{challenge.name}</h2>
-          <p className="text-xl">{challenge.description}</p>
-          <p className="text-xl mt-4">Fecha de inicio: {challenge.startDate}</p>
-          <p className="text-xl">Fecha de fin: {challenge.endDate}</p>
+          <div className="mt-4 flex flex-col">
+            <label htmlFor="name" className="text-black text-3xl font-bold mb-6">Nombre del reto</label>
+            <div className="text-2xl py-2 px-6 rounded-full bg-input-home w-full md:w-3/4">
+              {challenge.name}
+            </div>
+          </div> 
+          <div className="mt-4 flex flex-col">
+            <label htmlFor="description" className="text-black text-3xl font-bold mb-6">Descripción</label>
+            <div className="text-2xl py-2 px-6 rounded-full bg-input-home w-full md:w-3/4">
+              {challenge.description}
+            </div>
+          </div>
+         
         </div>
       ) : (
-        <p className="mt-6 text-xl">No hay un reto disponible para el mes actual.</p>
+        <p className="mt-6 text-3xl">No hay un reto disponible para el mes actual.</p>
       )}
     </div>
   );
