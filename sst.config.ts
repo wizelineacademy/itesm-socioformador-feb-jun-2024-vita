3785bf9e465816c52b369881235a91ac927e1b8b
julio2505 <juliomeza2510@outlook.com>
2024-06-01 23:19:54 -0600

    fix: Fixed sst configuration

--- sst.config.ts
+++ sst.config.ts
@@ -1,47 +1,46 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-
   app(input) {
     return {
-      name: "vita",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      home: "aws",
-    };
+      name: 'vita',
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      home: 'aws',
+    }
   },
 
   async run() {
-    const openApiKey = new sst.Secret("OpenApiKey");
-    const facebookId = new sst.Secret("FacebookId");
-    const facebookSecret = new sst.Secret("FacebookSecret");
-    const googleId = new sst.Secret("GoogleId");
-    const googleSecret = new sst.Secret("GoogleSecret");
-    const WebhookVerifyToken = new sst.Secret("WebhookVerifyToken");
-    const GraphApiToken = new sst.Secret("GraphApiToken");
-    const GeminiApiKey = new sst.Secret("GeminiApiKey")
+    const openApiKey = new sst.Secret('OpenApiKey')
+    const facebookId = new sst.Secret('FacebookId')
+    const facebookSecret = new sst.Secret('FacebookSecret')
+    const googleId = new sst.Secret('GoogleId')
+    const googleSecret = new sst.Secret('GoogleSecret')
+    const WebhookVerifyToken = new sst.Secret('WebhookVerifyToken')
+    const GraphApiToken = new sst.Secret('GraphApiToken')
+    const GeminiApiKey = new sst.Secret('GeminiApiKey')
 
-    const database = new sst.aws.Postgres("MyDatabase", {
+    const database = new sst.aws.Postgres('MyDatabase', {
       scaling: {
-        min: "0.5 ACU",
-        max: "1 ACU"
-      }
+        min: '0.5 ACU',
+        max: '1 ACU',
+      },
     })
 
-    const bucket = new sst.aws.Bucket("MyBucket", {
-      public: true
-    });
-
-    const blogCron = new sst.aws.Cron("MyCronJob", {
-      job: "src/cron_functions/blog.handler",
-      schedule: "cron(0 6 * * ? *)"
+    const bucket = new sst.aws.Bucket('MyBucket', {
+      public: true,
     })
 
-    const remindersCron = new sst.aws.Cron("RemindersJob", {
-      job: "src/cron_functions/reminders.handler",
-      schedule: "rate(30 minutes)"
+    const blogCron = new sst.aws.Cron('MyCronJob', {
+      job: 'src/cron_functions/blog.handler',
+      schedule: 'cron(0 6 * * ? *)',
     })
 
-    new sst.aws.Nextjs("MyWeb", {
+    const remindersCron = new sst.aws.Cron('RemindersJob', {
+      job: 'src/cron_functions/reminders.handler',
+      schedule: 'rate(30 minutes)',
+    })
+
+    new sst.aws.Nextjs('MyWeb', {
       link: [
         bucket,
         database,
@@ -54,15 +53,12 @@ export default $config({
         googleSecret,
         WebhookVerifyToken,
         GraphApiToken,
-        GeminiApiKey
+        GeminiApiKey,
       ],
       environment: {
         NEXTAUTH_URL: process.env.NEXTAUTH_URL!,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET!,
-        DATABASE_NAME: process.env.DATABASE_NAME!,
-        SERVICE_ARN: process.env.SERVICE_ARN!,
-        SECRET_ARN: process.env.SECRET_ARN!
-      }
-    });
+      },
+    })
   },
-});
+})
