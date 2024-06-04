e013a06cf464d900beeeb86f74ed0ffa861ff95f
julio2505 <juliomeza2510@outlook.com>
2024-06-03 22:06:28 -0600

    chore: Updated sst version

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -1,1 +1,54 @@
-/// <reference path="./.sst/types.generated.ts" />
+/* tslint:disable */
+/* eslint-disable */
+import "sst"
+declare module "sst" {
+  export interface Resource {
+    FacebookId: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+    FacebookSecret: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+    GeminiApiKey: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+    GoogleId: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+    GoogleSecret: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+    GraphApiToken: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+    MyBucket: {
+      name: string
+      type: "sst.aws.Bucket"
+    }
+    MyDatabase: {
+      clusterArn: string
+      database: string
+      secretArn: string
+      type: "sst.aws.Postgres"
+    }
+    MyWeb: {
+      type: "sst.aws.Nextjs"
+      url: string
+    }
+    OpenApiKey: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+    WebhookVerifyToken: {
+      type: "sst.sst.Secret"
+      value: string
+    }
+  }
+}
+export {}
