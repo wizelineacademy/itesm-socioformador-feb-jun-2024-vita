f251fb6750a132ddbe79e428b17bc3ccb3fc7fe7
julio2505 <juliomeza2510@outlook.com>
2024-06-06 20:26:41 -0600

    chore: Updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,20 @@
-export { default } from "next-auth/middleware"
+export { default } from 'next-auth/middleware'
 
 //Agregar rutas que deben ser protegidas
-export const config = { 
-    matcher: [
-        "/home",
-        "/home/:path",
-        "/nutrition",
-        "/nutrition/:path*",
-        "/exercise",
-        "/exercise/:path*",
-        "/conversation",
-        "/reminders",
-        "/reminders/:path*"
-    ] 
+export const config = {
+  matcher: [
+    '/home',
+    '/home/:path',
+    '/nutrition',
+    '/nutrition/:path*',
+    '/exercise',
+    '/exercise/:path*',
+    '/sleep',
+    '/sleep/:path*',
+    '/conversation',
+    '/reminders',
+    '/reminders/:path*',
+    '/social',
+    '/social/:path*',
+  ],
 }
