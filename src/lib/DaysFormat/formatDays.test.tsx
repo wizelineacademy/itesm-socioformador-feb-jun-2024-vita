32e0f4d08b4aa071b703a4a6631f4df2bfff5526
julio2505 <juliomeza2510@outlook.com>
2024-06-10 21:11:30 -0600

    fix: Fixed configuration of tests to use GMT timezone

--- src/lib/DaysFormat/formatDays.test.tsx
+++ src/lib/DaysFormat/formatDays.test.tsx
@@ -1,6 +1,6 @@
 import { formatDays } from './days'
 
-//For these tests the timezone is configured as GMT-6
+//For these tests the timezone is configured as GMT
 
 test('formats a typical date correctly', () => {
   const result = formatDays('2023-06-10T16:00:00Z')
@@ -9,12 +9,12 @@ test('formats a typical date correctly', () => {
 
 test('handles dates at the beginning of the month correctly', () => {
   const result = formatDays('2023-07-01T00:00:00Z')
-  expect(result).equal('30/06/2023')
+  expect(result).equal('01/07/2023')
 })
 
 test('handles dates at the beginning of the year correctly', () => {
   const result = formatDays('2023-01-01T00:00:00Z')
-  expect(result).equal('31/12/2022')
+  expect(result).equal('01/01/2023')
 })
 
 test('handles invalid date strings by returning "Invalid Date"', () => {
